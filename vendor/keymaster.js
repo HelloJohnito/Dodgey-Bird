b1f74000beef2b14dda70e63687bdb2c288cf5b8
John <johnitolee@gmail.com>
2017-04-19 13:14:41 -0700

    Update gravity and font

--- vendor/keymaster.js
+++ vendor/keymaster.js
@@ -205,8 +205,8 @@
   };
 
   // set the handlers globally on document
-  addEvent(document, 'keydown', function(event) { dispatch(event, _scope) }); // Passing _scope to a callback to ensure it remains the same by execution. Fixes #48
-  addEvent(document, 'keyup', clearModifier);
+  addEvent(document, 'keyup', function(event) { dispatch(event, _scope) }); // Passing _scope to a callback to ensure it remains the same by execution. Fixes #48
+  addEvent(document, 'keydown', clearModifier);
 
   // reset modifiers to false whenever the window is (re)focused.
   addEvent(window, 'focus', resetModifiers);
